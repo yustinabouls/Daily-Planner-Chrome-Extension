d99c368355967242cacb80ccd59e12e49f4a1b4c
yustinabouls <yustina.farag.bouls@gmail.com>
2023-06-07 12:17:22 -0400

    Step 3: fetch tasks from local storage

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const taskInput = document.getElementById("taskInput")
 const addTaskButton = document.getElementById("addTaskButton")
 const taskList = document.getElementById("taskList")
 
+
+function getTasksFromLocalStorage() {
+    return JSON.parse(localStorage.getItem("tasks")) || [];
+}
