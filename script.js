03053b84eb5e15d1badacb60de4880b253727553
yustinabouls <yustina.farag.bouls@gmail.com>
2023-06-07 12:26:26 -0400

    Step 2 completed: Loaded tasks from Local Storage

--- script.js
+++ script.js
@@ -6,3 +6,8 @@ const taskList = document.getElementById("taskList")
 function getTasksFromLocalStorage() {
     return JSON.parse(localStorage.getItem("tasks")) || [];
 }
+
+let taskArray = getTasksFromLocalStorage()
+
+
+
