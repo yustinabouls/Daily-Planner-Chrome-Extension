330302a3309ad3ac246cd08d8f8cb563624951df
yustinabouls <yustina.farag.bouls@gmail.com>
2023-06-08 12:34:21 -0400

    Step 8: Implemented the Render Tasks function

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ let taskArray = getTasksFromLocalStorage()
 
 // 3.2 Implement the updateTasksInLocalStorage() Function
 function updateTasksInLocalStorage(){
-localStorage.setItem(tasks, JSON.stringify())
+localStorage.setItem("tasks", JSON.stringify(taskArray))
 }
 
 // 3.3 Implement the createTask(taskText) Function
@@ -32,4 +32,55 @@ function deleteTask(index) {
     deleteTask.splice(index, 1)
     
     updateTasksInLocalStorage()
+    }
+
+
+
+// 3.5 Implement the createTaskElement(taskObj) Function
+ function createTaskElement(taskObj) {
+        const taskItem = document.createElement("li");
+        taskItem.classList.add("taskItem");
+      
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.classList.add("checkbox");
+        checkbox.checked = taskObj.completed;
+        checkbox.addEventListener("change", function () {
+          taskObj.completed = checkbox.checked;
+          taskTextElement.classList.toggle("completed", taskObj.completed);
+          updateTasksInLocalStorage();
+        });
+      
+        const taskTextElement = document.createElement("span");
+        taskTextElement.classList.add("taskText");
+        taskTextElement.textContent = taskObj.text;
+        taskTextElement.classList.toggle("completed", taskObj.completed);
+      
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("removeButton");
+        removeButton.textContent = "X";
+        removeButton.addEventListener("click", function () {
+          deleteTask(taskObj);
+          renderTasks();
+        });
+      
+        taskItem.appendChild(checkbox);
+        taskItem.appendChild(taskTextElement);
+        taskItem.appendChild(removeButton);
+      
+        return taskItem;
+    
+    }
+
+    // 4: Step 6: Implement the Render Tasks Function
+    function renderTasks() {
+        taskList.innerHTML = ""
+
+        for(let i = 0; i < taskArray.length; i++){
+            // create a const variable "taskElement" assign the function createTaskElement[taskArray[i]]
+            const taskElement = createTaskElement[taskArray[i]]
+
+            taskList.appendChild(taskElement);
+        }
+        
     }
