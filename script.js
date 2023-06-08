dd5fab8f4d82132912d5eeae47bd94dba3e077f2
yustinabouls <yustina.farag.bouls@gmail.com>
2023-06-08 10:27:51 -0400

    Step 5: Implemented delete task function

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+// Step 1: Define Essential HTML Elements in JavaScript
 const taskInput = document.getElementById("taskInput")
 const addTaskButton = document.getElementById("addTaskButton")
 const taskList = document.getElementById("taskList")
 
 
+
+// 3.1 Implement the getTasksFromLocalStorage() Function
 function getTasksFromLocalStorage() {
     return JSON.parse(localStorage.getItem("tasks")) || [];
 }
 
+// Step 2: Load Tasks from Local Storage
 let taskArray = getTasksFromLocalStorage()
 
+
+// 3.2 Implement the updateTasksInLocalStorage() Function
 function updateTasksInLocalStorage(){
 localStorage.setItem(tasks, JSON.stringify())
+}
+
+// 3.3 Implement the createTask(taskText) Function
+function createTask(taskText) {
+    let task = {
+        text: taskText,
+        completed: false
+    }
 }
